7a3cc450a45d3fc870fda50a19ea9b615f205d2c
Test <myspace.djn3o@gmail.com>
2012-04-10 18:03:37 +0300

    branch

--- test/src/com/git/test/TestActivity.java
+++ test/src/com/git/test/TestActivity.java
@@ -2,6 +2,7 @@ package com.git.test;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class TestActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +10,6 @@ public class TestActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Toast.makeText(this, "BRANCH", Toast.LENGTH_SHORT).show();
     }
 }
